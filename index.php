bae48d7e482ba62cb1fdda6886f3cde3ea57ca8e
KilianJ23 <kilian.janus@3wa.io>
2024-08-01 14:47:20 +0000

    Routeur et default controller

--- index.php
+++ index.php
@@ -6,3 +6,10 @@ require "vendor/autoload.php";
 // charge le contenu du .env dans $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+$route = null;
+if (isset($_GET['route'])) {
+    $route = $_GET['route'];
+}
+$router = new Router();
+$router->handleRequest($route);
--- services/Router.php
+++ services/Router.php
@@ -1,70 +1,22 @@
 <?php
 
-class Router
-{
-    private AuthController $ac;
-    private ForumController $fc;
-
+class Router {
+    
     public function __construct()
     {
-        $this->ac = new AuthController();
-        $this->fc = new ForumController();
     }
-    public function handleRequest(array $get) : void
-    {
-        if(!isset($get["route"]))
+
+    public function handleRequest(? string $route) : void {
+        
+        $dc = new DefaultController;
+        
+        if($route === null)
         {
-            $this->fc->home();
-        }
-        else if(isset($get["route"]) && $get["route"] === "register")
-        {
-            $this->ac->register();
-        }
-        else if(isset($get["route"]) && $get["route"] === "check-register")
-        {
-            $this->ac->checkRegister();
-        }
-        else if(isset($get["route"]) && $get["route"] === "login")
-        {
-            $this->ac->login();
-        }
-        else if(isset($get["route"]) && $get["route"] === "check-login")
-        {
-            $this->ac->checkLogin();
-        }
-        else if(isset($get["route"]) && $get["route"] === "logout")
-        {
-            $this->ac->logout();
-        }
-        else if(isset($get["route"]) && $get["route"] === "category")
-        {
-            if(isset($get["category_id"]))
-            {
-                $this->fc->category($get["category_id"]);
-            }
-            else
-            {
-                $this->fc->home();
-            }
-        }
-        else if(isset($get["route"]) && $get["route"] === "post")
-        {
-            if(isset($get["post_id"]))
-            {
-                $this->fc->post($get["post_id"]);
-            }
-            else
-            {
-                $this->fc->home();
-            }
-        }
-        else if(isset($get["route"]) && $get["route"] === "check-comment")
-        {
-            $this->fc->checkComment();
+            $dc->homepage();
         }
         else
         {
-            $this->fc->home();
+            $dc->notFound();
         }
     }
 }
